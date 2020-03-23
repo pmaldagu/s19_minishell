462eee1a14b6b9720ba49266adb1582cf4492419
pmaldagu <pierremaldague@gmail.com>
2020-03-23 15:49:53 +0100

    base

---
+++ minishell.h
@@ -0,0 +1,31 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "./libft/libft.h"
+
+typedef struct	s_data
+{
+	char	*cmd;
+	char	*opt;
+	char	*path;
+	char	*result;
+	char	pipe;
+}		t_data;
+/*
+** gnl
+int	ft_strlen_end(char const *s);
+int	ft_strchr_pos(const char *s, int c);
+int	get_next_line(char **line);
+*/
+/*
+** parsing
+*/
+int	ft_pars(char *line);
+int	no_cmd(char *line);
+
+int	ft_echo(char *line);
+
+#endif
---
+++ srcs/parsing.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmaldagu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/23 14:15:12 by pmaldagu          #+#    #+#             */
+/*   Updated: 2020/03/23 15:38:36 by pmaldagu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	no_cmd(char *line)
+{
+	int i;
+
+	i = 0;
+	write (0, "minishell: command not found: ", 30);
+	while (ft_isalpha(line[i]))
+	{
+		write(0, &line[i], 1);
+		i++;
+	}
+	write (0, "\n", 1);
+	return (0);
+}
+
+int	ft_pars(char *line)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (line[i] == 32)
+		i++;
+	j = i;
+	while (ft_isalpha(line[j]))
+	{
+		line[j] = ft_tolower(line[j]);
+		j++;
+	}
+	if (!(ft_strncmp(&line[i], "echo", 4)))
+		return (ft_echo(&line[i]));
+	else if (!(ft_strncmp(&line[i], "cd", 2)))
+		return (2);
+	else if (!(ft_strncmp(&line[i], "pwd", 3)))
+		return (3);
+	else if (!(ft_strncmp(&line[i], "export", 6)))
+		return (4);
+	else if (!(ft_strncmp(&line[i], "unset", 5)))
+		return (5);
+	else if (!(ft_strncmp(&line[i], "env", 3)))
+		return (6);
+	else if (!(ft_strncmp(&line[i], "exit", 4)))
+		return (7);
+	else
+		return (no_cmd(&line[i]));
+}
